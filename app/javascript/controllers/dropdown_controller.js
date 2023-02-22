9a06190f249f84b6b1ea07c1a8d4f3601360b19a
Johan Walter <walter.johan25@gmail.com>
2023-02-22 15:57:52 +0100

    add turbo frame to the edit functions load and note

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,13 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  static targets = ['menu']
-
-  connect() {
-    console.log("je suis dans le controller")
-  }
+  static targets = ['menu', 'overlay' ]
 
   toggle() {
+    console.log('coucou');
+    const overlay = document.querySelector('.overlay');
+    const dropdown = document.querySelector('#menu');
+
+    console.dir(overlay);
+    console.dir(dropdown);
+
     this.menuTarget.classList.toggle('hidden');
-  }
+
+    console.log(dropdown.classList.contains('hidden'));
+
+    setTimeout(() => {
+      if (!dropdown.classList.contains('hidden')){
+        console.log('caca');
+        overlay.addEventListener('click', event => {
+        dropdown.classList.add('hidden');
+        });
+      };
+    }, 3);
+  };
 }
