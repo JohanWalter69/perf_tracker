962e9e2ec082d9400c3ba98b4820ed565ead9fd0
Johan Walter <walter.johan25@gmail.com>
2023-03-02 10:00:22 +0100

    perf slider done js / css ok // need to work on the responsive

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,22 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+$("#slider").roundSlider({
+  sliderType: "min-range",
+  editableTooltip: false,
+  radius: 140,
+  width: 25,
+  circleShape: "half-top",
+  value: 40,
+  lineCap: "round",
+  mouseScrollAction: true,
+  // Ligne ci dessous a refactoriser : comment sortir la fonction
+  tooltipFormat: function (e) {
+    var userLoad = $('#load').data('url');
+    var val = e.value
+
+    return val + "%" + "<div>" + userLoad*val/100 + " kg" + "<div>";
+  },
+
+});
